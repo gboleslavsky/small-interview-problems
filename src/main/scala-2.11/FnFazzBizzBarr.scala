fe97c11b7bbaca3a92ac4a7d327825d0723c4e18
gboleslavsky <gregboleslavsky@gmail.com>
2016-07-12 19:49:58 -0400

    added an implicit for clearer syntax

--- src/main/scala-2.11/FnFazzBizzBarr.scala
+++ src/main/scala-2.11/FnFazzBizzBarr.scala
@@ -11,22 +11,23 @@ multiples of 7 output Barr.
 Multiples of several of these output each word in turn on the same line
 (for example, line 21 should output "Fazz Barr").Write a test for this.  Do not spend more than 30 minutes on this.
 */
+  implicit class ModInt(x: Int) {
+    //for easy to read infix syntax
+    def divisibleBy(y: Int) = x % y == 0
+  }
 
   def fazzBizzBarr(ints: Seq[Int] ): Seq[String] = {
-
-    def divBy(i: Int, d: Int): Boolean = i % d == 0
-
     (ints) map
       (i =>
         i match {
-          case i if (divBy(i, 3) && divBy(i, 5) && divBy(i, 7)) => " Fazz Bizz Barr "
-          case i if (divBy(i, 3) && divBy(i, 5))                => " Fazz Bizz "
-          case i if (divBy(i, 3) && divBy(i, 7))                => " Fazz Barr "
-          case i if (divBy(i, 5) && divBy(i, 7))                => " Bizz Barr "
-          case i if divBy(i, 3)                                 => " Fazz "
-          case i if divBy(i, 5)                                 => " Bizz "
-          case i if divBy(i, 7)                                 => " Barr "
-          case i                                                => s"  $i  "
+          case i if (i divisibleBy 3 && i divisibleBy 5 && i divisibleBy 7)   => " Fazz Bizz Barr "
+          case i if (i divisibleBy 3 && i divisibleBy 5)                      => " Fazz Bizz "
+          case i if (i divisibleBy 3 && i divisibleBy 7)                      => " Fazz Barr "
+          case i if (i divisibleBy 5 && i divisibleBy 7)                      => " Bizz Barr "
+          case i if i divisibleBy 3                                           => " Fazz "
+          case i if i divisibleBy 5                                           => " Bizz "
+          case i if i divisibleBy 7                                           => " Barr "
+          case i                                                              => s"  $i  "
         })
   }
 
