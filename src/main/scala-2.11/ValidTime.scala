b978a89f35979ec4ca426002c4fdf8861dd73db3
gboleslavsky <gregboleslavsky@gmail.com>
2017-01-25 00:24:40 -0500

    reformatted code for readability, aded 1 test, needs a few more

--- src/main/scala-2.11/ValidTime.scala
+++ src/main/scala-2.11/ValidTime.scala
@@ -6,23 +6,33 @@
   * this problem was part of several exercises and the only one i did not finish in time. I had an hour for three
   * problems, two were very simple, this one I ran out of time and wasn't sure how best to solve it.
   * Solution that appears below came to me the next day, after I tried and failed to find a reasonable one on the web.
+  * I am not sure there is a more expressive solution in another language, this one appears pretty transparent and simple
+  * and i think shows the good parts of Scala
   */
+
 object ValidTime {
+
   val INVALID_TIME = "No valid time"
+
   def solution(a: Int, b: Int, c: Int, d: Int): String = {
     val time = validTime(a,b,c,d)
-    if (time == INVALID_TIME) INVALID_TIME
-    time.substring(0,2)  + ":" + time.substring(2)
+    if (time == INVALID_TIME)
+      INVALID_TIME
+    else
+      time.substring(0,2)  + ":" + time.substring(2)
   }
 
   def validTime(a: Int, b: Int, c: Int, d: Int): String = {
-    val allPossible = List(a, b, c, d).permutations.map(p => p.mkString).toSet
-    val minutes = List.range(0, 60).map(n => f"$n%02d").toSet
-    val hours = List.range(0, 24).map(n => f"$n%02d").toSet
-    val allValid = ((for (h <- hours; m <- minutes) yield h + m))
-    val possibleAndValid = allValid.intersect(allPossible)
+    val possible =          List(a, b, c, d).permutations.map(p => p.mkString).toSet
+    val minutes =           List.range(0, 60).map(n => f"$n%02d").toSet
+    val hours =             List.range(0, 24).map(n => f"$n%02d").toSet
+    val valid =             (for (h <- hours; m <- minutes) yield h + m)
+    val possibleAndValid =  valid.intersect(possible)
 
-    if (possibleAndValid.isEmpty) INVALID_TIME else possibleAndValid.max
+    if (possibleAndValid.isEmpty)
+      INVALID_TIME
+    else
+      possibleAndValid.max
   }
 
 }
